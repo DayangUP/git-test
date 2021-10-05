939a3787044b4d7f637a1e6fe543c900b28722f4
dayang <zdayangz@gmail.com>
2021-10-05 15:58:38 +0800

    second commit

--- src/main/java/com/dayang/git/GitTest.java
+++ src/main/java/com/dayang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dayang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
